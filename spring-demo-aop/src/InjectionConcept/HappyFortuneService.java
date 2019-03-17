ba9d3f08bf6be47f83a5b435b7838e2855020250
karthikeyan <karthikeyan@ADMINRG-3AANNC1>
2019-03-17 11:26:35 +0530

    syso update

--- spring-demo-aop/src/InjectionConcept/HappyFortuneService.java
+++ spring-demo-aop/src/InjectionConcept/HappyFortuneService.java
@@ -5,6 +5,7 @@ public class HappyFortuneService implements FortuneService {
 	@Override
 	public String getFortune() {
 		// TODO Auto-generated method stub
+		System.out.println("fortune");
 		return "good";
 	}
 
